76d1cf3d3dada8e4266d9547f145bf88f1a1b6d4
DieryValencia <diery.valencia@campusucc.edu.co>
2025-01-27 22:49:40 -0500

    IUserRepository

---
+++ crud/src/main/java/com/api/crud/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.api.crud.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+
+public class UserController {
+
+    
+
+}
---
+++ crud/src/main/java/com/api/crud/models/UserModel.java
@@ -0,0 +1,61 @@
+package com.api.crud.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "users") // This tells Hibernate to make a table out of this class
+
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String firsName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(int id) {  // Long o int revisar 
+        this.id = id;
+    }
+
+    public String getFirsName() {
+        return firsName;
+    }   
+
+    public void setFirsName(String firsName) {
+        this.firsName = firsName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+}
